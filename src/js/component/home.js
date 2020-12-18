b38390ac34213015cd1581443486e8d249b406c9
kparra7491 <kparra7491@gmail.com>
2020-12-18 05:56:31 +0000

    updated

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,57 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { component } from "react";
 
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			list: [],
+			currentItem: ""
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<ol>
+					<li>
+						<div>
+							<input
+								type="text"
+								ref={el => (this.input = el)}
+								onChange={e =>
+									this.setState({
+										currentItem: e.target.value
+									})
+								}
+							/>
+							<a
+								ref={el => (this.plus = el)}
+								onClick={() =>
+									this.setState({
+										list: [
+											...this.state.list,
+											this.state.currentItem
+										]
+									})
+								}>
+								<i className="fas fa-plus" />
+							</a>
+						</div>
+					</li>
+					{this.state.list.map((item, i) => (
+						<li key={i}>
+							{item}
+							<a
+							//can't get a .splice() to work 	onClick={e =>this.setState({ people: })}
+							>
+								<i className="fas fa-minus" />
+							</a>
+						</li>
+					))}
+				</ol>
+			</div>
+		);
+	}
 }
