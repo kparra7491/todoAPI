119256dfd5f206aea234ed2fbf166fa3b5bbbcf4
kparra7491 <kparra7491@gmail.com>
2020-12-18 20:54:00 +0000

    api fetch added

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -7,16 +7,32 @@ export class Home extends React.Component {
 
 		this.state = {
 			list: [],
-			listRefs: [],
-			currentItem: ""
+
+			currentItem: "",
+			url: "https://assets.breatheco.de/apis/fake/todos/user/kparra"
 		};
 	}
 
 	addItem = () => {
 		if (this.state.currentItem) {
+			var newListObject = { label: this.state.currentItem, done: false };
+			var updatedList = this.state.list.concat(newListObject);
 			this.setState({
-				list: [...this.state.list, this.state.currentItem]
+				list: updatedList
 			});
+
+			fetch(this.state.url, {
+				method: "PUT", // or 'POST'
+				body: JSON.stringify(updatedList), // data can be `string` or {object}!
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(res => res.json())
+				.then(response =>
+					console.log("Success:", JSON.stringify(response))
+				)
+				.catch(error => console.error("Error:", error));
 		}
 	};
 
@@ -35,6 +51,25 @@ export class Home extends React.Component {
 	// 		? (contentStyle.display = "block")
 	// 		: (contentStyle.display = "none");
 	// };
+
+	componentDidMount() {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/kparra")
+			.then(function(response) {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.then(jsonifiedResponse => {
+				console.log(jsonifiedResponse);
+
+				this.setState({ list: jsonifiedResponse });
+			})
+
+			.catch(function(error) {
+				console.log("ya broke it", error);
+			});
+	}
 
 	render() {
 		return (
@@ -64,13 +99,14 @@ export class Home extends React.Component {
 					</li>
 					{this.state.list.map((item, i) => (
 						<li
+							ref={el => (this.li = el)}
 							value={i}
 							className="listTop d-flex justify-content-between"
 							key={i}
 							// onMouseOver={this.unhide()}
 							//ref={el => (this.listRefs[i] = el)}
 						>
-							{item}
+							{item.label}-{item.done}
 							<a onClick={() => this.removeItem(i)}>
 								<i className="fas fa-minus" />
 							</a>
